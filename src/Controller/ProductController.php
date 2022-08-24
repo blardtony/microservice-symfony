6882657f356a9003d77e62ac0eb9f67d1dde65e0
Tony BLARD <tony.blard@orange.com>
2022-08-24 16:47:38 +0200

    Use custom Serializer for DTO in ProductController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,14 +2,38 @@
 
 namespace App\Controller;
 
+use App\DTO\LowestPriceEnquiry;
+use App\Service\Serializer\DTOSerializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/product', name: 'product_')]
 class ProductController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index()
+    #[Route('/{id}/lowest-price', name: 'lowest-price', methods: ['POST'])]
+    public function lowestPrice(Request $request, int $id, DTOSerializer $serializer): Response
     {
+        if ($request->headers->has('force-fail')) {
+            return new JsonResponse(
+                [
+                    'error' => 'Promotions Engine failure message'
+                ],
+                $request->headers->get('force_fail')
+            );
+        }
+
+        /** @var LowestPriceEnquiry $lowestPriceEnquiry */
+        $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
+
+        $responseContent = $serializer->serialize($lowestPriceEnquiry, 'json');
+
+        return new Response(
+            $responseContent,
+            200
+        );
     }
 }
