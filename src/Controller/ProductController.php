ef7e6ed911bbb00d6f8255dea941e98e10c354c1
Tony BLARD <tony.blard@orange.com>
2022-08-25 11:18:59 +0200

    Fix params

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -46,9 +46,9 @@ class ProductController extends AbstractController
 
         $promotions = $this->promotionRepository->findValidForProduct($product, date_create_immutable($lowestPriceEnquiry->getRequestDate()));
 
-        $modifiedEnquiry = $promotionsFilter->apply($lowestPriceEnquiry, $promotions);
+        $modifiedEnquiry = $promotionsFilter->apply($lowestPriceEnquiry, ...$promotions);
 
-        $responseContent = $serializer->serialize($lowestPriceEnquiry, 'json');
+        $responseContent = $serializer->serialize($modifiedEnquiry, 'json');
 
         return new Response(
             $responseContent,
