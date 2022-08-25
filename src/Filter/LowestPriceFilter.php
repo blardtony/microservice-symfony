a81166519731f9b527c88684406e0d98ed5411d6
Tony BLARD <tony.blard@orange.com>
2022-08-25 14:21:52 +0200

    Change typos : promotion to promotions

--- src/Filter/LowestPriceFilter.php
+++ src/Filter/LowestPriceFilter.php
@@ -7,18 +7,26 @@ namespace App\Filter;
 use App\DTO\LowestPriceEnquiry;
 use App\DTO\PromotionEnquiryInterface;
 use App\Entity\Promotion;
+use App\Filter\Modifier\Factory\PriceModifierFactoryInterface;
 
 class LowestPriceFilter implements PromotionsFilterInterface
 {
 
     /**
      * @param LowestPriceEnquiry $enquiry
-     * @param Promotion ...$promotion
+     * @param Promotion ...$promotions
      * @return PromotionEnquiryInterface
      */
-    public function apply(PromotionEnquiryInterface $enquiry, Promotion ...$promotion): PromotionEnquiryInterface
+    public function apply(PromotionEnquiryInterface $enquiry, Promotion ...$promotions): PromotionEnquiryInterface
     {
-        $enquiry->setDiscountedPrice(50);
+
+        $price = $enquiry->getProduct()->getPrice();
+        $quantity = $enquiry->getQuantity();
+        $lowestPrice = $quantity * $price;
+
+//        $priceModifier = ;
+//        $modifiedPrice = $priceModifier->modifiy($price, $quantity, $promotion, $enquiry);
+        $enquiry->setDiscountedPrice(250);
         $enquiry->setPrice(100);
         $enquiry->setPromotionId(3);
         $enquiry->setPromotionName('Black Friday half price sale');
