e3968948a7fe598b3602b2dd0aec0226a293285a
Tony BLARD <tony.blard@orange.com>
2022-08-25 16:01:12 +0200

    Solid : interface Segregation

--- src/DTO/PriceEnquiryInterface.php
+++ src/DTO/PriceEnquiryInterface.php
@@ -6,5 +6,9 @@ namespace App\DTO;
 
 interface PriceEnquiryInterface extends PromotionEnquiryInterface
 {
+    public function setPrice(int $price);
 
+    public function setDiscountedPrice(float $discountedPrice);
+
+    public function getQuantity(): ?int;
 }
