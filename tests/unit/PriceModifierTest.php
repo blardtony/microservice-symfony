c46803e8eb09fec338663aa6ad90a8cd91ecd2d3
Tony BLARD <tony.blard@orange.com>
2022-08-25 15:03:45 +0200

    Create Test for EvenItemMultiplier

--- tests/unit/PriceModifierTest.php
+++ tests/unit/PriceModifierTest.php
@@ -9,6 +9,7 @@ use App\Entity\Product;
 use App\Entity\Promotion;
 use App\Filter\LowestPriceFilter;
 use App\Filter\Modifier\DateRangeMultiplier;
+use App\Filter\Modifier\EvenItemMultiplier;
 use App\Filter\Modifier\FixedPriceVoucher;
 use App\Tests\ServiceTestCase;
 
@@ -47,25 +48,20 @@ class PriceModifierTest extends ServiceTestCase
         $this->assertEquals(500, $modifiedPrice);
     }
 
+    public function testEvenItemMultiplierReturnCorrectlyModifiedPrice()
+    {
+        $enquiry = new LowestPriceEnquiry();
+        $enquiry->setQuantity(5);
 
-//    private function promotionsDataProvider(): array
-//    {
-//        $promotionOne = (new Promotion())->setName('Black Friday half price sale')
-//            ->setAdjustment(0.5)
-//            ->setCriteria(["form" => "2022-11-25", "to" => "2022-11-28"])
-//            ->setType("date_range_multiplier");
-//
-//        $promotionTwo = (new Promotion())->setName('Voucher OU812')
-//            ->setAdjustment(100)
-//            ->setCriteria(["code" => "OU812"])
-//            ->setType("fixed_price_voucher");
-//
-//        $promotionThree = (new Promotion())->setName('Buy one get one free')
-//            ->setAdjustment(0.5)
-//            ->setCriteria(["minimum_quantity" => "2"])
-//            ->setType("even_items_multiplier");
-//
-//        return [$promotionOne, $promotionTwo, $promotionThree];
-//    }
+        $promotion = (new Promotion())->setName('Buy one get one free')
+            ->setAdjustment(0.5)
+            ->setCriteria(["minimum_quantity" => 2])
+            ->setType("even_items_multiplier");
+
+        $evenItemMultiplier = new EvenItemMultiplier();
+        $modifiedPrice = $evenItemMultiplier->modify(100, 5, $promotion, $enquiry);
+
+        $this->assertEquals(300, $modifiedPrice);
+    }
 
 }
